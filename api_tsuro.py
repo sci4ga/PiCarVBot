45a53fc019c9b9899cca1953ac3851740ef2b2b6
andrewtsai <andrew.tsai@turner.com>
2019-07-12 14:19:06 -0400

    test-wheels seeing if it works with the import at root file level

--- api_tsuro.py
+++ api_tsuro.py
@@ -6,25 +6,25 @@ This is the tsuro module and supports all the ReST actions for the
 from flask import make_response
 # import native modules
 import logging
+import picar
+from picar import back_wheels, front_wheels
+
+picar.setup()
+db_file = "/Users/andrewtsai/personal-projects/RobotTsuro/config"
+fw = front_wheels.Front_Wheels(debug=False, db=db_file)
+bw = back_wheels.Back_Wheels(debug=False, db=db_file)
+bw.ready()
+fw.ready()
+
+SPEED = 60
+bw_status = 0
 
 def get_start():
     """
     This function responds to a request for /tsuro/start
     with a '200' upon successful startup
     """
-
-    import picar
-    from picar import back_wheels, front_wheels
-
-    picar.setup()
-    db_file = "/Users/andrewtsai/personal-projects/RobotTsuro/config"
-    fw = front_wheels.Front_Wheels(debug=False, db=db_file)
-    bw = back_wheels.Back_Wheels(debug=False, db=db_file)
-    bw.ready()
-    fw.ready()
-
-    SPEED = 60
-    bw_status = 0
+    global SPEED, bw_status
     bw.speed = SPEED
     bw.forward()
     bw_status = 1
@@ -36,18 +36,8 @@ def get_stop():
     This function responds to a request for /tsuro/stop
     with a '200' upon successful stop
     """
-    import picar
-    from picar import back_wheels, front_wheels
-
-    picar.setup()
-    db_file = "/Users/andrewtsai/personal-projects/RobotTsuro/config"
-    fw = front_wheels.Front_Wheels(debug=False, db=db_file)
-    bw = back_wheels.Back_Wheels(debug=False, db=db_file)
-    bw.ready()
-    fw.ready()
-
-    SPEED = 60
-    bw_status = 0
+    
+    global SPEED, bw_status
     bw.stop()
     bw_status = 0
 
